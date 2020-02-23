fcf8fd525897c46a082cb59402b936a5e3973863
Andrzej Podgórski <andrzej.podgorski@hotmail.com>
2020-02-23 22:19:55 +0100

    Added swagger

--- ExpenseTrack/ExpenseTrackAPI/Startup.cs
+++ ExpenseTrack/ExpenseTrackAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ExpenseTrackAPI
 {
@@ -36,11 +37,22 @@ namespace ExpenseTrackAPI
                     .Migrate();
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExpenseTrack API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExpenseTrack API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
